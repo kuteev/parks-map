048a9687a61dbf478efd24f7c2071737eef28f88
Marat Kuteev <kuteev@gmail.com>
2019-06-27 15:08:00 -0400

    ui updates

--- src/app/parks/parks.component.ts
+++ src/app/parks/parks.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'parks',
-    templateUrl: './parks.component.html'
+    templateUrl: './parks.component.html',
+    styleUrls: ['./parks.component.css'],
 })
 
 export class ParksComponent { }
