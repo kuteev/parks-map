141c234f7d2780ef68ea81e2565dbd18c3ba5ca8
Marat Kuteev <kuteev@gmail.com>
2019-06-27 17:23:28 -0400

    unit test

--- src/app/google-maps/google-maps.service.spec.ts
+++ src/app/google-maps/google-maps.service.spec.ts
@@ -1,26 +1,35 @@
 import { TestBed, async } from '@angular/core/testing';
 import { GoogleMapsService } from './google-maps.service';
 import { ParkItemContainer } from './park-item-container.model';
+import { ParkItem } from '../models/park-item';
 
+declare let google: any;
 
 describe('GoogleMapsService', () => {
     let googleMapsService: GoogleMapsService;
     const mockDocument: any = {};
 
-
     beforeEach(() => {
+        window['google'] = { maps: {} };
         googleMapsService = new GoogleMapsService(mockDocument);
+        googleMapsService['listeners'] = [];
 
         // stub map object
         googleMapsService['map'] = {
-            addListener: (eventName, handler) => { },
-            getBounds: () => new google.maps.LatLngBounds(),
-            getCenter: () => new google.maps.LatLng(100, 100),
-            setCenter: (latLng) => {}
-         } as google.maps.Map;
+            addListener: (eventName, handler) => {
+                googleMapsService['listeners'].push(handler);
+            },
+            getBounds: () => null,
+            getCenter: () => null,
+            setCenter: (latLng) => { }
+        } as google.maps.Map;
 
         // stub placesService object
-        googleMapsService['placesService'] = { } as google.maps.places.PlacesService;
+        googleMapsService['placesService'] = {
+            textSearch: () => null
+        };
+
+
 
     });
 
@@ -30,6 +39,18 @@ describe('GoogleMapsService', () => {
     });
 
     it('searchParks should work', () => {
+        let result: ParkItem[] = [];
+        jasmine.clock().install();
+
+        googleMapsService.searchParks().subscribe((parkItems) => {
+            result = parkItems;
+
+        });
+
+        googleMapsService['listeners'][0]([new ParkItem()]);
+        jasmine.clock().tick(1000);
+        expect(result.length).toBe(2);
+
 
     });
 
