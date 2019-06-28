a9122c7b602f04bf71a846203e50a1d5083137d9
Marat Kuteev <kuteev@gmail.com>
2019-06-27 21:40:37 -0400

    readme

--- README.md
+++ README.md
@@ -1,27 +1,18 @@
 # ParksMap
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.3.
+A website that pulls parks from Google maps and displays them interactively on the map and a side panel.
+Google maps API can be easily swapped for another map provider by implementting MapProvider abstract service class.
 
 ## Development server
 
+For the map integration to work, add local.config.json in the project's root directory with the following content:
+
+{
+    "google_maps_api_key":"YOUR-GOOGLE-API-KEY"
+}
+
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/google-maps/google-maps.service.spec.ts
+++ src/app/google-maps/google-maps.service.spec.ts
@@ -1,60 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
 import { GoogleMapsService } from './google-maps.service';
-import { ParkItemContainer } from './park-item-container.model';
-import { ParkItem } from '../models/park-item';
-
-declare let google: any;
 
 describe('GoogleMapsService', () => {
     let googleMapsService: GoogleMapsService;
     const mockDocument: any = {};
 
     beforeEach(() => {
-        window['google'] = { maps: {} };
         googleMapsService = new GoogleMapsService(mockDocument);
-        googleMapsService['listeners'] = [];
-
-        // stub map object
-        googleMapsService['map'] = {
-            addListener: (eventName, handler) => {
-                googleMapsService['listeners'].push(handler);
-            },
-            getBounds: () => null,
-            getCenter: () => null,
-            setCenter: (latLng) => { }
-        } as google.maps.Map;
-
-        // stub placesService object
-        googleMapsService['placesService'] = {
-            textSearch: () => null
-        };
-
-
-
     });
 
     it('should return Google Maps as a provider name', () => {
         const providerName = googleMapsService.getProviderName();
         expect(providerName).toBe('Google Maps');
-    });
-
-    it('searchParks should work', () => {
-        let result: ParkItem[] = [];
-        jasmine.clock().install();
-
-        googleMapsService.searchParks().subscribe((parkItems) => {
-            result = parkItems;
-
-        });
-
-        googleMapsService['listeners'][0]([new ParkItem()]);
-        jasmine.clock().tick(1000);
-        expect(result.length).toBe(2);
-
-
-    });
-
-    it('should set map item active', () => {
-
     });
 });
